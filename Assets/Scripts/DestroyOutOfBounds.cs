9ce9db5c5337e5f346ffe2b91b67f200603f8b46
rowanspain <125788793+rowanspain@users.noreply.github.com>
2023-09-21 21:32:40 -0700

    LessonD

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,6 +22,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if(transform.position.z < bottomOfScene)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
     }
 }
